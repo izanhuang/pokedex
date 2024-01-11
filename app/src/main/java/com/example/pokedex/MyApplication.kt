ff929d8b6482cc0bc0e327fddbd4c0c3441da7f2
izanhuang <izan.8259@gmail.com>
2024-01-11 15:28:25 -0500

    Removed unused value

--- app/src/main/java/com/example/pokedex/MyApplication.kt
+++ app/src/main/java/com/example/pokedex/MyApplication.kt
@@ -3,14 +3,9 @@ package com.example.pokedex
 import android.app.Application
 import com.example.pokedex.di.AppComponent
 import com.example.pokedex.di.DaggerAppComponent
-import com.example.pokedex.prefdatastore.DataStoreManager
 
 open class MyApplication : Application() {
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.factory().create(applicationContext)
-    }
-
-    open val dataStoreManager by lazy {
-        DataStoreManager(this)
     }
 }
