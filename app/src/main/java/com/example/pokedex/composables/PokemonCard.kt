48892c12e1cd6288620f3d28a8b0883a6368cc42
izanhuang <izan.8259@gmail.com>
2024-01-15 13:29:22 -0500

    Handled case when pokemon sprite is null

--- app/src/main/java/com/example/pokedex/composables/PokemonCard.kt
+++ app/src/main/java/com/example/pokedex/composables/PokemonCard.kt
@@ -1,11 +1,12 @@
 package com.example.pokedex.composables
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -13,9 +14,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.pokedex.R
@@ -46,25 +49,43 @@ fun PokemonCard(
             Text(
                 text = "NO. ${pokemon.id.toString()}",
                 textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
                 style = MaterialTheme.typography.labelLarge,
                 fontWeight = FontWeight.Bold
             )
             // TODO: Handle case where sprite is null or there is no image url
-            AsyncImage(
-                model = pokemon.sprites?.frontDefault,
-                contentDescription = stringResource(R.string.pokemon_card),
-                modifier = Modifier
-                    .padding(vertical = 16.dp)
-                    .defaultMinSize(minWidth = 200.dp, minHeight = 144.dp)
-            )
+            if (pokemon.sprites?.frontDefault != null) {
+                AsyncImage(
+                    model =
+                    pokemon.sprites.frontDefault,
+                    contentDescription = stringResource(R.string.pokemon_card),
+                    modifier = Modifier
+                        .padding(vertical = 16.dp)
+                        .size(width = 200.dp, height = 144.dp)
+                )
+            } else {
+                Image(
+                    painterResource(
+                        R.drawable.question_mark
+                    ),
+                    contentDescription = stringResource(R.string.pokemon_card),
+                    modifier = Modifier
+                        .padding(vertical = 48.dp)
+                        .size(width = 200.dp, height = 80.dp)
+
+                )
+            }
             pokemon.displayName?.let {
                 Text(
                     text = it.uppercase(),
                     textAlign = TextAlign.Center,
                     modifier = Modifier.fillMaxWidth(),
                     style = MaterialTheme.typography.titleMedium,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
