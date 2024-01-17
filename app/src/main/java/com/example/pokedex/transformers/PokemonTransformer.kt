a19a73da1389668d4840865316f874cf1939ab50
izanhuang <izan.8259@gmail.com>
2024-01-17 10:51:11 -0500

    Added safeLet function check to PokemonTransformer

--- app/src/main/java/com/example/pokedex/transformers/PokemonTransformer.kt
+++ app/src/main/java/com/example/pokedex/transformers/PokemonTransformer.kt
@@ -6,6 +6,22 @@ import com.example.pokedex.types.PokemonSprites
 import com.example.pokedex.types.PokemonSpritesFromNetwork
 
 class PokemonTransformer {
+    inline fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, R : Any> safeLet(
+        p1: T1?,
+        p2: T2?,
+        p3: T3?,
+        p4: T4?,
+        p5: T5?,
+        block: (T1, T2, T3, T4, T5) -> R?
+    ): R? {
+        return if (p1 != null && p2 != null && p3 != null && p4 != null && p5 != null) block(
+            p1,
+            p2,
+            p3,
+            p4,
+            p5,
+        ) else null
+    }
 
     private fun convertPokemonSpritesFromNetworkToPokemonSprites(
         pokemonSpritesFromNetwork: PokemonSpritesFromNetwork
@@ -20,21 +36,27 @@ class PokemonTransformer {
         pokemonFromNetwork: PokemonFromNetwork?
     ): Pokemon? {
         if (pokemonFromNetwork != null) {
-            val displayName = pokemonFromNetwork.name?.replaceFirstChar { char -> char.uppercase()  }
-
-            return Pokemon(
-                id = pokemonFromNetwork.id,
-                name = pokemonFromNetwork.name,
-                displayName = displayName,
-                baseExperience = pokemonFromNetwork.base_experience,
-                height = pokemonFromNetwork.height,
-                weight = pokemonFromNetwork.weight,
-                sprites = pokemonFromNetwork.sprites?.let {
-                    convertPokemonSpritesFromNetworkToPokemonSprites(
-                        it
-                    )
-                }
-            )
+            val maybeDisplayName = pokemonFromNetwork.name?.replaceFirstChar { char -> char.uppercase() }
+            return safeLet(
+                pokemonFromNetwork.id,
+                pokemonFromNetwork.name,
+                pokemonFromNetwork.height,
+                pokemonFromNetwork.weight,
+                maybeDisplayName
+            ) {
+                id, name, height, weight, displayName ->
+                Pokemon(
+                    id = id,
+                    name = name,
+                    displayName = displayName,
+                    baseExperience = pokemonFromNetwork.base_experience,
+                    height = height,
+                    weight = weight,
+                    sprites = pokemonFromNetwork.sprites?.let {
+                       convertPokemonSpritesFromNetworkToPokemonSprites(it)
+                    }
+                )
+            }
         }
 
         return null
